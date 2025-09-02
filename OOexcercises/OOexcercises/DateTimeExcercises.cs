d7d8b2c43fbd60acdc5869c95f96e63f9ad49bc9
Cedric <cedric.coussement@gmail.com>
2025-09-02 23:29:35 +0200

    H10-birthday gedaan

--- OOexcercises/OOexcercises/DateTimeExcercises.cs
+++ OOexcercises/OOexcercises/DateTimeExcercises.cs
@@ -12,13 +12,17 @@ namespace OOexcercises
         {
 
             Console.WriteLine("Uit te voeren oefening?" +
-             "\n1. H10-Clock");
+             "\n1. H10-Clock" +
+             "\n2. H10_Birthday");
             int userInput = Convert.ToInt32(Console.ReadLine());
 
             switch (userInput)
             {
                 case 1:
                     h10_clock();
+                    break;
+                case 2:
+                    H10_Birthday();
                     break;
                 default:
                     Console.WriteLine("Onbekende keuze");
@@ -29,11 +33,22 @@ namespace OOexcercises
             while (true)
             {
                 Console.Clear();
-             DateTime now = DateTime.Now;
+                DateTime now = DateTime.Now;
                 Console.WriteLine(now.ToString("T"));
                 System.Threading.Thread.Sleep(1000);
 
             }
+        }
+        public static void H10_Birthday()
+        {
+            DateTime now = DateTime.Now;
+            Console.Write("Voer je verjaardag in:");
+            string date = Console.ReadLine();
+            DateTime birthday = DateTime.Parse(date);
+            DateTime nextBirtday = new DateTime(now.Year, birthday.Month, birthday.Day);
+            nextBirtday = nextBirtday.AddYears(1);
+            TimeSpan diff = nextBirtday - now;
+            Console.WriteLine($"Over {diff.Days} dagen bent u jarig");
         }
     }
 }
