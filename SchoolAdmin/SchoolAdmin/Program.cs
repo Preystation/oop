55c9a7e106cd9d341a08db74b026a42a90ddd24c
Cedric <cedric.coussement@gmail.com>
2025-09-03 16:19:25 +0200

    H10_1 SchoolAdmin gemaakt

--- SchoolAdmin/SchoolAdmin/Program.cs
+++ SchoolAdmin/SchoolAdmin/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Student s1 = new Student();
+            Student s2 = new Student();
+            s1.Name = "Said Aziz";
+            s2.Name = " Mieke Vermeulen";
+            s1.Courses.Add("Programmeren ");
+            s1.Courses.Add("Databanken ");
+            s2.Courses.Add("Communicatie ");
+            s1.Birthdate = new DateTime(2000, 06, 01);
+            s2.Birthdate = new DateTime(1998, 01, 01);
+            s1.StudentCounter++;
+            s2.StudentCounter++;
+            Console.WriteLine($"Said: {s1.Name}, geboren {s1.Birthdate.ToString("d")} met studentennummber {s1.StudentCounter}; Said volgt {s1.Courses[0]} en {s1.Courses[1]} ");
+            Console.WriteLine($"Mieke: {s2.Name}, geboren {s2.Birthdate.ToString("d")} met studentennummber {s2.StudentCounter}; Said volgt {s2.Courses[0]}");
+
         }
     }
 }
---
+++ SchoolAdmin/SchoolAdmin/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAdmin
+{
+     class Student
+    {
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+        public uint StudentNumber { get; set; }
+      public  List<string> Courses = new List<string>();
+
+        public uint StudentCounter = 1;
+
+    }
+}
