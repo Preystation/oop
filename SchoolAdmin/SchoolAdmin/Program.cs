9841697237c1c36aaf6c7d611fd6e7629c8935b2
Cedric <cedric.coussement@gmail.com>
2025-09-03 15:59:09 +0200

    schoolAdmin folder aangemaakt

---
+++ SchoolAdmin/SchoolAdmin/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolAdmin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
