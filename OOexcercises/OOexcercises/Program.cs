6882f60c48bcd65dd32ce404baba5cca2796f843
Cedric <cedric.coussement@gmail.com>
2025-09-04 22:59:41 +0200

    con oef

--- OOexcercises/OOexcercises/Program.cs
+++ OOexcercises/OOexcercises/Program.cs
@@ -4,14 +4,42 @@ namespace OOexcercises
 {
     internal class Program
     {
+        public class Dier
+        {
+            public string Soort;
+            public int Leeftijd;
 
+
+            public Dier(string soort,int leeftijd)
+            {
+                this.Soort = soort;
+                this.Leeftijd = leeftijd;
+                if (leeftijd < 0) Leeftijd = 0;
+            }
+            public Dier() : this("Hond", 1)
+            {
+
+            }
+            public Dier(string soort) : this(soort, 0)
+            {
+
+            }
+            public void ToonInfo()
+            {
+                Console.WriteLine("Soort: " + Soort + ", Leeftijd: " + Leeftijd);
+            }
+        }
 
         
         static void Main(string[] args)
         {
+            Dier d1 = new Dier("Vogelbekdier", 10);
+            d1.ToonInfo();
 
-
-
+            Dier d2 = new Dier();
+            Dier d3 = new Dier("Haai");
+            d2.ToonInfo();
+            d3.ToonInfo();
         }
     }
 }
