b149c9c7953838138f4a96a895841603ce3a1a70
Cedric <cedric.coussement@gmail.com>
2025-09-06 23:23:07 +0200

    fzf

--- OOexcercises/OOexcercises/Program.cs
+++ OOexcercises/OOexcercises/Program.cs
@@ -4,6 +4,27 @@ namespace OOexcercises
 {
     internal class Program
     {
+        public class Media
+        {
+            public string Titel;
+
+            public void ToonInfo()
+            {
+                Console.WriteLine($"{Titel}");
+            }
+            public Media(string titel)
+            {
+                Titel = titel;
+            }
+        }
+      public class Boeken : Media
+        {
+            public string Auteurs;
+            public Boeken(string titel, string auteurs) : base(titel)
+            {
+                Auteurs = auteurs;
+            }
+        }
         public class Dier
         {
             public string Soort;
@@ -40,6 +61,18 @@ namespace OOexcercises
             Dier d3 = new Dier("Haai");
             d2.ToonInfo();
             d3.ToonInfo();
+
+            Dictionary<int, string> costumers = new Dictionary<int, string>();
+            costumers.Add(1, "Cedric");
+            costumers.Add(2, "Bart");
+            costumers.Add(3, "Kai");
+            costumers.Add(4, "Jan");
+
+            foreach(var costumer in costumers)
+            {
+                Console.WriteLine($"{costumer.Key}\t {costumer.Value}");
+            }
+            Console.WriteLine(costumers[1]);
         }
     }
 }
