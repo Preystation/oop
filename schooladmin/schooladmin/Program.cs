84dd86dd54999958ce7be61b4289fb2c91e97ca2
Coussement Cédric (s157201) <cedric.coussement@student.ap.be>
2025-09-16 17:01:38 +0200

    H10_1 SchoolAdmin

---
+++ schooladmin/schooladmin/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace schooladmin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student said = new Student();
+            Student mieke = new Student();
+            said.Name = "Said Aziz";
+            said.Birthday = new DateTime(2000, 06, 01);
+            said.Courses.Add("Programmeren");
+            said.Courses.Add("Databanken");
+
+            mieke.Name = "Mieke Vermeulen";
+            mieke.Birthday = new DateTime(1998, 01, 01);
+            mieke.Courses.Add("Communicatie");
+            
+
+            Console.WriteLine($"said {said.Name} geboren {said.Birthday.ToString("d")} met studentnummer {Student.StudentCounter++}; Said volgt {said.Courses[0]} en {said.Courses[1]}");
+            Console.WriteLine($"mieke {mieke.Name} geboren {mieke.Birthday.ToString("d")} met studentnummer {Student.StudentCounter++}; Mieke volgt {mieke.Courses[0]}");
+        }
+
+    }
+}
---
+++ schooladmin/schooladmin/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schooladmin
+{
+    internal class Student
+    {
+        public string Name;
+        public DateTime Birthday;
+        public uint StudentNumber;
+        public List<string> Courses = new List<string>();
+        public static uint StudentCounter = 1;
+    }
+}
