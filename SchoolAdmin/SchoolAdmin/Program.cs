176990d21d9109f4a3f78226263accb54fcf6714
Cedric <cedric.coussement@gmail.com>
2025-10-03 11:33:22 +0200

    H10_1 SchoolAdmin Student attributen,H10_2 SchoolAdmin, H10_3 SchoolAdmin

---
+++ SchoolAdmin/SchoolAdmin/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace SchoolAdmin
+{
+    internal class Program
+    {
+        public static void DemoStudents()
+        {
+            Student said = new Student();
+            said.Name = " Said Aziz";
+            Student mieke = new Student();
+            mieke.Name = "Mieke Vermeulen";
+            Console.WriteLine(said.GenerateNameCard());
+            Console.WriteLine(mieke.GenerateNameCard());
+        }
+        static void Main(string[] args)
+        {
+            Student said = new Student();
+            said.Name = " Said Aziz";
+            said.Birthdate = new DateTime(2000, 06, 01);
+            said.Courses.Add("Programmeren");
+            said.Courses.Add("Databanken");
+
+            Student mieke = new Student();
+            mieke.Name = "Mieke Vermeulen";
+            mieke.Birthdate = new DateTime(1998, 01, 01);
+            mieke.Courses.Add("Communicatie");
+
+            int studentnumber = Student.StudentCounter;
+
+            Console.WriteLine($"{said.Name}, geboren {said.Birthdate.ToString("d")} met studentnummer {studentnumber++}; Said volgt {said.Courses[0]} en {said.Courses[1]}");
+            Console.WriteLine($"{mieke.Name}, geboren {mieke.Birthdate.ToString("d")} met studentnummer {studentnumber++}; Mieke volgt {mieke.Courses[0]}");
+
+            Console.WriteLine("Wat wil je doen?");
+            int userCoice = Convert.ToInt32(Console.ReadLine());
+            switch (userCoice)
+            {
+                case 1:
+                    DemoStudents();
+                    break;
+
+                default:
+                    Console.WriteLine("Keuze is niet herkent");
+                    break;
+            }
+        
+        
+        }
+    }
+}
---
+++ SchoolAdmin/SchoolAdmin/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAdmin
+{
+    internal class Student
+    {
+        public string Name;
+        public DateTime Birthdate;
+        public int StudentNumber;
+        private List<string> courses = new List<string>();
+        public static int StudentCounter = 1;
+
+
+        public string GenerateNameCard()
+        {
+            return $"{Name} (STUDENT)";
+        }
+
+        public byte DetermineWorkLoad()
+        {
+            return (byte)(courses.Count()*10);
+        }
+
+        public void RegisterForCourse(string course)
+        {
+            foreach(string item in courses)
+            {
+                if(item == course)
+                {
+                    Console.WriteLine($"{Name} is al ingeschreven voor deze cursus.");
+                }
+                else
+                {
+                    courses.Add(course);
+                }
+            }
+        }
+    }
+}
